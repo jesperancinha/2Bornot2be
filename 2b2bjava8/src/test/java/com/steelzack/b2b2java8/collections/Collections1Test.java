38b2758339c5a49bf51dca0e91f3572993bf8fee
Joao <jofisaes@hotmail.com>
2016-04-06 09:40:18 +0200

    SZ-N More tests using streams for colections

--- 2b2bjava8/src/test/java/com/steelzack/b2b2java8/collections/Collections1Test.java
+++ 2b2bjava8/src/test/java/com/steelzack/b2b2java8/collections/Collections1Test.java
@@ -2,15 +2,42 @@ package com.steelzack.b2b2java8.collections;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by joaofilipesabinoesperancinha on 06-04-16.
  */
 public class Collections1Test {
+    private List<String> testArrayOfStrings = Arrays.asList(//
+            "Eucaliptus", //
+            "Mint", //
+            "Car", //
+            "Uncle Bob", //
+            "Developers Developers", //
+            "No hands", //
+            "Pertinent"
+    );
     @Test
     public void getStringsThatStartWith() throws Exception {
-        final Collections1 collections1 = new Collections1();
+        final Collections1 collections1 = new Collections1(testArrayOfStrings);
 
-        collections1.getStringsThatStartWith();
+        List<String> result1 = collections1.getStringsThatStartWith("E");
+
+        assertEquals("Eucaliptus",result1.get(0));
+        assertEquals(1, result1.size());
     }
 
+    @Test
+    public void getStringsThatContain() throws Exception {
+        final Collections1 collections1 = new Collections1(testArrayOfStrings);
+
+        List<String> result1 = collections1.getStringsThatContain("er");
+
+        assertEquals("Developers Developers",result1.get(0));
+        assertEquals("Pertinent",result1.get(1));
+        assertEquals(2, result1.size());
+    }
 }
