d056e3e4196b793d0a60b67619dd6df8a572eddf
Joao <jofisaes@hotmail.com>
2016-04-09 22:52:30 +0200

    SZ-N adds SH1 resolution

--- 2b2bsecurity/src/test/java/com/steelzack/b2b2java8/HashingManagerTest.java
+++ 2b2bsecurity/src/test/java/com/steelzack/b2b2java8/HashingManagerTest.java
@@ -9,10 +9,17 @@ import static org.junit.Assert.assertEquals;
  */
 public class HashingManagerTest {
     @Test
-    public void getHashedString() throws Exception {
+    public void getHashStringSHA1() throws Exception {
+        final HashingManager hashingManager = new HashingManager();
+        final String hashedString = hashingManager.getHashStringSHA1("The quick brown fox jumps over the lazy dog");
+        assertEquals("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12", hashedString);
+    }
+
+    @Test
+    public void getHashedStringMD5() throws Exception {
         final HashingManager hashingManager = new HashingManager();
 
-        final String hashedString = hashingManager.getHashedString("The quick brown fox jumps over the lazy dog");
+        final String hashedString = hashingManager.getHashedStringMD5("The quick brown fox jumps over the lazy dog");
         assertEquals("9e107d9d372bb6826bd81d3542a419d6", hashedString);
     }
 }
