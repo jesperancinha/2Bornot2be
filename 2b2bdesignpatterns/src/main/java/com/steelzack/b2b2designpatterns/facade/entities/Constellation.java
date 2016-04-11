c8025e4f965d96835cac326846e06ad7f9b82f06
Joao <jofisaes@hotmail.com>
2016-04-11 23:08:26 +0200

    SZ-N Facade design pattern

---
+++ 2b2bdesignpatterns/src/main/java/com/steelzack/b2b2designpatterns/facade/entities/Constellation.java
@@ -0,0 +1,8 @@
+package com.steelzack.b2b2designpatterns.facade.entities;
+
+/**
+ * Created by joaofilipesabinoesperancinha on 11-04-16.
+ */
+public interface Constellation {
+    void drawCostellation();
+}
