7aab55ee2a62e1bf2e66f09930643e47e71f503d
Joao <jofisaes@hotmail.com>
2016-04-23 20:44:57 +0200

    SZ-N Log testing class improved

--- 2b2bjava8/src/test/java/com/steelzack/b2b2java8/functional/interfaces/BiConsumer2b2Test.java
+++ 2b2bjava8/src/test/java/com/steelzack/b2b2java8/functional/interfaces/BiConsumer2b2Test.java
@@ -5,9 +5,6 @@ import org.junit.Test;
 
 import java.util.function.BiConsumer;
 
-import static org.hamcrest.core.StringContains.containsString;
-import static org.junit.Assert.assertThat;
-
 /**
  * Created by joaofilipesabinoesperancinha on 23-04-16.
  */
@@ -34,34 +31,12 @@ public class BiConsumer2b2Test extends LoggerInfoTest {
         final BiConsumer<String, String> consumer2String = biConsumer2b21.getBiConsumer2();
         consumer1String.andThen(consumer2String).accept("string1", "string2");
 
-        final String logString = flushLogs();
-        assertThat(logString, containsString(BI_CONSUMER2B2_NUMBER_1_IS_DONE_2_3));
-        assertThat(logString, containsString(BI_CONSUMER2B2_NUMBER_2_IS_DONE_2_3));
-        assertThat(logString, containsString(BI_CONSUMER2B2_NUMBER_1_IS_DONE_STRING1_STRING2));
-        assertThat(logString, containsString(BI_CONSUMER2B2_NUMBER_2_IS_DONE_STRING1_STRING2));
-
         assertLogOrder( //
-                logString, //
                 BI_CONSUMER2B2_NUMBER_1_IS_DONE_2_3, //
                 BI_CONSUMER2B2_NUMBER_2_IS_DONE_2_3, //
                 BI_CONSUMER2B2_NUMBER_1_IS_DONE_STRING1_STRING2, //
                 BI_CONSUMER2B2_NUMBER_2_IS_DONE_STRING1_STRING2 //
         ); //
-    }
-
-    private void assertLogOrder(
-            String logString,
-            String biConsumer2b2Number1IsDone23,
-            String biConsumer2b2Number2IsDone23,
-            String biConsumer2b2Number1IsDoneString1String2,
-            String biConsumer2b2Number2IsDoneString1String2) {
-        final String orderLogString = logString.replace("\n", "");
-        final String segment1 = orderLogString.split(biConsumer2b2Number1IsDone23)[1];
-        assertThat(segment1, containsString(biConsumer2b2Number2IsDone23));
-        final String segment2 = segment1.split(biConsumer2b2Number2IsDone23)[1];
-        assertThat(segment2, containsString(biConsumer2b2Number1IsDoneString1String2));
-        final String segment3 = segment2.split(biConsumer2b2Number1IsDoneString1String2)[1];
-        assertThat(segment3, containsString(biConsumer2b2Number2IsDoneString1String2));
     }
 
     @Test
@@ -76,15 +51,7 @@ public class BiConsumer2b2Test extends LoggerInfoTest {
         final BiConsumer<String, String> consumer2String = biConsumer2b21.getBiConsumer2();
         consumer2String.andThen(consumer1String).accept("string1", "string2");
 
-
-        final String logString = flushLogs();
-        assertThat(logString, containsString(BI_CONSUMER2B2_NUMBER_2_IS_DONE_2_3));
-        assertThat(logString, containsString(BI_CONSUMER2B2_NUMBER_1_IS_DONE_2_3));
-        assertThat(logString, containsString(BI_CONSUMER2B2_NUMBER_2_IS_DONE_STRING1_STRING2));
-        assertThat(logString, containsString(BI_CONSUMER2B2_NUMBER_1_IS_DONE_STRING1_STRING2));
-
         assertLogOrder( //
-                logString, //
                 BI_CONSUMER2B2_NUMBER_2_IS_DONE_2_3, //
                 BI_CONSUMER2B2_NUMBER_1_IS_DONE_2_3, //
                 BI_CONSUMER2B2_NUMBER_2_IS_DONE_STRING1_STRING2, //
