77993cb721dc783d3041b3ebf7ebf43c9ee60358
Joao <jofisaes@hotmail.com>
2016-04-24 15:26:38 +0200

    SZ-N Adds potential energy calculation with the use of JScience

--- 2b2bjava8/src/main/java/com/steelzack/b2b2java8/functional/interfaces/BiFunction2b2.java
+++ 2b2bjava8/src/main/java/com/steelzack/b2b2java8/functional/interfaces/BiFunction2b2.java
@@ -1,7 +1,26 @@
 package com.steelzack.b2b2java8.functional.interfaces;
 
+import java.util.function.BiFunction;
+
 /**
  * Created by joaofilipesabinoesperancinha on 23-04-16.
  */
 public class BiFunction2b2 {
+    /**
+     * This is the actual BiFunction to calculate the potential energy of an element considering earth's gravity
+     */
+    final BiFunction<Double, Double, Double> potentialEnergyCalculator =
+            (inHeight, inMass) -> inMass * inHeight * org.jscience.physics.amount.Constants.g.getEstimatedValue();
+
+    /**
+     * Returns potential energy in Joules
+     *
+     * @param height In meters
+     * @param mass   In Kilograms
+     * @return
+     */
+    public Double calculatePotentialEnergy(Double height, Double mass) {
+
+        return potentialEnergyCalculator.apply(height, mass);
+    }
 }
