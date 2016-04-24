a1af33ea1cf9101ba45aae65fa48f71974dd36eb
Joao <jofisaes@hotmail.com>
2016-04-24 15:27:09 +0200

    SZ-N Code cleanup

--- 2b2bjava8/src/main/java/com/steelzack/b2b2java8/functional/interfaces/BiFunction2b2.java
+++ 2b2bjava8/src/main/java/com/steelzack/b2b2java8/functional/interfaces/BiFunction2b2.java
@@ -2,6 +2,8 @@ package com.steelzack.b2b2java8.functional.interfaces;
 
 import java.util.function.BiFunction;
 
+import static org.jscience.physics.amount.Constants.g;
+
 /**
  * Created by joaofilipesabinoesperancinha on 23-04-16.
  */
@@ -10,7 +12,7 @@ public class BiFunction2b2 {
      * This is the actual BiFunction to calculate the potential energy of an element considering earth's gravity
      */
     final BiFunction<Double, Double, Double> potentialEnergyCalculator =
-            (inHeight, inMass) -> inMass * inHeight * org.jscience.physics.amount.Constants.g.getEstimatedValue();
+            (inHeight, inMass) -> inMass * inHeight * g.getEstimatedValue();
 
     /**
      * Returns potential energy in Joules
