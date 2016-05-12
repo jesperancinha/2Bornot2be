58bb654736ac6bb4f94d90c181de972027932dc1
Joao FSE <jofisaes@hotmail.com>
2016-05-12 19:16:17 +0200

    SZ-N Temporal adjusters

--- 2b2bjava8/src/main/java/com/steelzack/b2b2java8/utils/Date.java
+++ 2b2bjava8/src/main/java/com/steelzack/b2b2java8/utils/Date.java
@@ -1,13 +1,17 @@
 package com.steelzack.b2b2java8.utils;
 
+import static java.time.DayOfWeek.MONDAY;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 
 /**
  * Created by jesperancinha on 12-5-16.
  */
-public class DateToMicrosecondsFormat {
+public class Date {
 
     private static final DateTimeFormatter FROM_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH+mm+ss+SSSSSS");
     private static final DateTimeFormatter TO_FORMATTER = DateTimeFormatter.ofPattern("SSSSSS");
@@ -15,11 +19,15 @@ public class DateToMicrosecondsFormat {
     private static final SimpleDateFormat FROM_SIMPLE = new SimpleDateFormat("yyyy-MM-dd-HH+mm+ss+SSSSSS");
     private static final SimpleDateFormat TO_SIMPLE = new SimpleDateFormat("SSSSSS");
 
-    public static String fromDateToDate(String srcDate) {
+    public static String fromDateToMicrosecondsDate(String srcDate) {
         return TO_FORMATTER.format(FROM_FORMATTER.parse(srcDate));
     }
 
-    public static String fromDateToDateSimple(String srcDate) throws ParseException {
+    public static String fromDateToMicrosecondsDateSimple(String srcDate) throws ParseException {
         return TO_SIMPLE.format(FROM_SIMPLE.parse(srcDate));
+    }
+
+    public static LocalDate getStartOfCurrentYearLastWeek(LocalDate currentDate) {
+        return currentDate.with(TemporalAdjusters.lastDayOfYear()).with(TemporalAdjusters.previousOrSame(MONDAY));
     }
 }
