3d0408ddc47037f747b9bb1260658e60096c8c16
Joao FSE <jofisaes@hotmail.com>
2016-05-28 18:04:30 +0200

    SZ-N Test for double sized map

--- 2b2bjava8/src/test/java/com/steelzack/b2b2java8/collections/FullSetRetrieval2WithLambdaMethodTest.java
+++ 2b2bjava8/src/test/java/com/steelzack/b2b2java8/collections/FullSetRetrieval2WithLambdaMethodTest.java
@@ -2,6 +2,7 @@ package com.steelzack.b2b2java8.collections;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -29,6 +30,36 @@ public class FullSetRetrieval2WithLambdaMethodTest {
         fullLeaves.stream().forEach(stringTreeNode -> System.out.println(stringTreeNode.getName()));
 
         assertThat(fullLeaves, hasSize(3000));
+    }
+
+    @Test
+    public void getAllLeaves2Parallel() throws Exception {
+
+        final FullSetRetrieval2WithLambdaMethod.TreeNode<String> rootNode = new FullSetRetrieval2WithLambdaMethod.TreeNode<>("ROOT");
+        final FullSetRetrieval2WithLambdaMethod.TreeNode<String> treeNode = new FullSetRetrieval2WithLambdaMethod.TreeNode<>("ROOT");
+        final FullSetRetrieval2WithLambdaMethod.TreeNode<String> treeNode2 = new FullSetRetrieval2WithLambdaMethod.TreeNode<>("ROOT");
+        rootNode.setChildrenStrem(Arrays.asList(treeNode, treeNode2).stream());
+        FullSetRetrieval2WithLambdaMethod.TreeNode<String> currentTreeNode = treeNode;
+        FullSetRetrieval2WithLambdaMethod.TreeNode<String> currentTreeNode2 = treeNode2;
+        add10Leaves(currentTreeNode, 0);
+        add10Leaves(currentTreeNode2, 0);
+
+        for (int i = 1; i < 100; i++) {
+            final FullSetRetrieval2WithLambdaMethod.TreeNode<String> newTeeNode = new FullSetRetrieval2WithLambdaMethod.TreeNode<>("CHILD" + i);
+            final FullSetRetrieval2WithLambdaMethod.TreeNode<String> newTeeNode2 = new FullSetRetrieval2WithLambdaMethod.TreeNode<>("CHILD" + i);
+            currentTreeNode.setChildrenStrem(Stream.of(newTeeNode));
+            currentTreeNode2.setChildrenStrem(Stream.of(newTeeNode2));
+            add10Leaves(currentTreeNode, i);
+            add10Leaves(currentTreeNode2, i);
+            currentTreeNode = newTeeNode;
+            currentTreeNode2 = newTeeNode2;
+        }
+
+        Set<FullSetRetrieval2WithLambdaMethod.TreeNode<String>> fullLeaves = FullSetRetrieval2WithLambdaMethod.getAllLeaves(rootNode);
+
+        fullLeaves.stream().forEach(stringTreeNode -> System.out.println(stringTreeNode.getName()));
+
+        assertThat(fullLeaves, hasSize(6000));
     }
 
     private void add10Leaves(FullSetRetrieval2WithLambdaMethod.TreeNode<String> treeNode, int iteration) {
