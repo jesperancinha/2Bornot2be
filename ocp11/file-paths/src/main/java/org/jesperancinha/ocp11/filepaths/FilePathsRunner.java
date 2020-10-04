a4538d6af0ceeb514ff5915c7375927263576fd5
Joao Esperancinha <jofisaes@gmail.com>
2020-10-04 16:16:49 +0200

    🗄 More file exercises

--- ocp11/file-paths/src/main/java/org/jesperancinha/ocp11/filepaths/FilePathsRunner.java
+++ ocp11/file-paths/src/main/java/org/jesperancinha/ocp11/filepaths/FilePathsRunner.java
@@ -8,8 +8,24 @@ import java.nio.file.Paths;
  */
 public class FilePathsRunner {
     public static void main(String[] args) {
-        Path p1 = Paths.get("/code/java/pathTest.java");
-        System.out.println(p1.getName(2).toString());
-        System.out.println(p1.getName(3).toString());
+
+        System.out.println("Exercise-1");
+        try {
+            Path p1 = Paths.get("/code/java/pathTest.java");
+            System.out.println(p1.getName(2).toString());
+            System.out.println(p1.getName(3).toString());
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.toString());
+        }
+
+        System.out.println("\nExercise-2");
+        Path p2 = Paths.get("/home/../temp/test.txt");
+        System.out.println(p2.normalize().toUri());
+
+
+        System.out.println("\nExercise-3");
+        Path p3 = Paths.get("/home/test");
+        System.out.println(p3.getName(0).toString());
     }
+
 }
