39c87efef4ddac9edac657b1f50a8bfed25829d4
Joao Esperancinha <jofisaes@gmail.com>
2020-10-06 08:22:19 +0200

    ☕️ A Java compile module example

---
+++ ocp11/java-compile/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module java {
+    requires java.base;
+    exports org.jesperancinha.ocp11.java.compile;
+}
---
+++ ocp11/java-compile/src/main/java/org/jesperancinha/ocp11/java/compile/JavaCompileRunner.java
@@ -0,0 +1,7 @@
+package org.jesperancinha.ocp11.java.compile;
+
+public class JavaCompileRunner {
+    public static void main(String[] args) {
+
+    }
+}
