2ab7258638a68eda14112b3d2da903298d82276f
Joao Esperancinha <jofisaes@gmail.com>
2020-10-13 22:26:42 +0200

    📁 Completes exercise with ServiceLoader

--- ocp11/static-modular-filter/usage/src/main/java/org/jesperancinha/ocp11/staticmodular/usage/UsageRunner.java
+++ ocp11/static-modular-filter/usage/src/main/java/org/jesperancinha/ocp11/staticmodular/usage/UsageRunner.java
@@ -1,10 +1,17 @@
 package org.jesperancinha.ocp11.staticmodular.usage;
 
+import org.jesperancinha.ocp11.staticmodular.api.Filter;
 import org.jesperancinha.ocp11.staticmodular.filter.GreatFilter;
+
+import java.util.ServiceLoader;
 
 public class UsageRunner {
     public static void main(String[] args) {
-        GreatFilter filter  = new GreatFilter();
-        System.out.printf("This is your filter %s", filter);
+        GreatFilter filter = new GreatFilter();
+        System.out.printf("This is your filter %s\n", filter);
+        System.out.println("Now let's try and load the service");
+        ServiceLoader<Filter> filterServiceLoader = ServiceLoader.load(Filter.class);
+        System.out.printf("This is your filter %s\n", filterServiceLoader.iterator().next());
+
     }
 }
