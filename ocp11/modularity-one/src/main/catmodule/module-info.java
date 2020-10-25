23d998f72752d130958a1687ff7dabca9249b44a
Joao Esperancinha <jofisaes@gmail.com>
2020-10-25 13:59:03 +0100

    🅾 Optional

--- ocp11/modularity-one/src/main/catmodule/module-info.java
+++ ocp11/modularity-one/src/main/catmodule/module-info.java
@@ -7,7 +7,10 @@ import org.jesperancinha.oc11.modularity.one.Feline;
  * 2. Exports will export and make available all classes to whatever module that uses this module.
  * 3. Provides will provide an implementation of {@link Feline} for the {@link java.util.ServiceLoader}.
  * 4. We using Cat as the implementation for our service {@link Cat}
- * 5. We cannot export individual classes in modules. Only packages
+ * 5. We cannot export individual classes in modules. Only packages.
+ * 6. Packages are exported and modules are required.
+ * 7. It is not possible to export individual classes.
+ * 8. Wildcards are not allowed in module definitions
  */
 module catmodule {
     uses Feline;
