21d77b4d878aa12336937eedec3eff664aea5143
Joao Esperancinha <jofisaes@gmail.com>
2020-10-28 23:28:22 +0100

    📓 Mabinogion Constant Switch

---
+++ ocp11-scale-up/cpf-switches/src/main/java/org/jesperancinha/ocp11/cpf/switches/SwitchesRunner.java
@@ -0,0 +1,11 @@
+package org.jesperancinha.ocp11.cpf.switches;
+
+public class SwitchesRunner {
+    public static void main(String[] args) {
+        switch ("Mabinogion"){
+            case "Mabinogion": System.out.println("GOTCHA!!");
+            default: System.out.println("You escape this time!! BONUS: Guess which number...");
+        }
+
+    }
+}
