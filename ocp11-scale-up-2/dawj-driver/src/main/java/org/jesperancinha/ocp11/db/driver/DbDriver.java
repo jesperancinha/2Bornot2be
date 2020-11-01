16e2d7347513b99a9821b5e5500a2f75529f9ac9
Joao Esperancinha <jofisaes@gmail.com>
2020-11-01 12:25:54 +0100

    🍃 JDBC 4.0 Driver module

---
+++ ocp11-scale-up-2/dawj-driver/src/main/java/org/jesperancinha/ocp11/db/driver/DbDriver.java
@@ -0,0 +1,18 @@
+package org.jesperancinha.ocp11.db.driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbDriver {
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.
+                    getConnection("jdbc:h2:mem:", "sa", "");
+            System.out.printf("Connection: %s / %s\n", conn.getMetaData().getDatabaseProductName(), conn.getCatalog());
+            System.out.printf("Current auto-commit setting: %s\n", conn.getAutoCommit());
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
