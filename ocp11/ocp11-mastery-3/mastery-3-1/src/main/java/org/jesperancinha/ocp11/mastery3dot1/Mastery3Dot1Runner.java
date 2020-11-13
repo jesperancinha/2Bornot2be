df94f3c42327a994e4741b086191d009101e4bae
Joao Esperancinha <jofisaes@gmail.com>
2020-11-13 19:15:01 +0100

    ⚔️ Mastery mastery-3-1 Singing in dutch - Foundation classes

---
+++ ocp11/ocp11-mastery-3/mastery-3-1/src/main/java/org/jesperancinha/ocp11/mastery3dot1/Mastery3Dot1Runner.java
@@ -0,0 +1,13 @@
+package org.jesperancinha.ocp11.mastery3dot1;
+
+import org.jesperancinha.console.consolerizer.Consolerizer;
+
+import static org.jesperancinha.console.consolerizer.Consolerizer.printBlueGenericLn;
+
+public class Mastery3Dot1Runner {
+    public static void main(String[] args) {
+        Consolerizer.typingWaitGlobal = 0;
+
+        printBlueGenericLn("================== Master Module mastery-3-1 ==================");
+    }
+}
