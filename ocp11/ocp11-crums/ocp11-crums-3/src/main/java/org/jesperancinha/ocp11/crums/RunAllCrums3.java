feedc4c92283f65ff93ee6ea54bb667ebde53b06
Joao Esperancinha <jofisaes@gmail.com>
2020-12-19 18:55:54 +0100

    ⚔️ Mastery 5 - Crum 23 - Parsers return type

--- ocp11/ocp11-crums/ocp11-crums-3/src/main/java/org/jesperancinha/ocp11/crums/RunAllCrums3.java
+++ ocp11/ocp11-crums/ocp11-crums-3/src/main/java/org/jesperancinha/ocp11/crums/RunAllCrums3.java
@@ -16,6 +16,7 @@ import org.jesperancinha.ocp11.crums.crum2.Crum2;
 import org.jesperancinha.ocp11.crums.crum20.Crum20;
 import org.jesperancinha.ocp11.crums.crum21.Crum21;
 import org.jesperancinha.ocp11.crums.crum22.Crum22;
+import org.jesperancinha.ocp11.crums.crum23.Crum23;
 import org.jesperancinha.ocp11.crums.crum3.Crum3;
 import org.jesperancinha.ocp11.crums.crum4.Crum4;
 import org.jesperancinha.ocp11.crums.crum5.Crum5;
@@ -57,5 +58,6 @@ public class RunAllCrums3 {
         Crum20.main(args);
         Crum21.main(args);
         Crum22.main(args);
+        Crum23.main(args);
     }
 }
