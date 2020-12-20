20beb80bd867c036ed4b6d4b00bdf9771cddbf04
Joao Esperancinha <jofisaes@gmail.com>
2020-12-20 10:58:37 +0100

    ⚔️ Mastery 5 - Crum 31 - File permissions

--- ocp11/ocp11-crums/ocp11-crums-3/src/main/java/org/jesperancinha/ocp11/crums/crum31/Crum31.java
+++ ocp11/ocp11-crums/ocp11-crums-3/src/main/java/org/jesperancinha/ocp11/crums/crum31/Crum31.java
@@ -1,11 +1,11 @@
 package org.jesperancinha.ocp11.crums.crum31;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.security.AccessControlContext;
+import java.security.AccessControlException;
 import java.security.AccessController;
 import java.security.Permission;
 import java.security.PermissionCollection;
@@ -17,6 +17,8 @@ import java.util.function.Function;
 import static org.jesperancinha.console.consolerizer.Consolerizer.printBlueGenericTitleLn;
 import static org.jesperancinha.console.consolerizer.Consolerizer.printGreenGenericLn;
 import static org.jesperancinha.console.consolerizer.Consolerizer.printOrangeGenericLn;
+import static org.jesperancinha.console.consolerizer.Consolerizer.printRedGeneric;
+import static org.jesperancinha.console.consolerizer.Consolerizer.printRedGenericLn;
 import static org.jesperancinha.console.consolerizer.Consolerizer.printRedThrowableAndExit;
 
 public class Crum31 {
@@ -29,16 +31,26 @@ public class Crum31 {
                 final var oos = new ObjectOutputStream(fos);
                 oos.writeUTF("You spin me round round baby round round like a record");
                 oos.flush();
-            } catch (FileNotFoundException e) {
-               printRedThrowableAndExit(e);
             } catch (IOException e) {
-               printRedThrowableAndExit(e);
+                printRedThrowableAndExit(e);
+            } catch (java.security.AccessControlException e) {
+                printRedGenericLn("This is expected! We are not allowing to write to file -> %s", e);
             }
             return "Ok!";
         };
         final String pathname = "/tmp/Crum31.txt";
         File f = new File(pathname);
 
+        final SecurityManager securityManager = System.getSecurityManager();
+        SecurityManager setSecurityManager = new SecurityManager() {
+            @Override
+            public void checkSecurityAccess(String target) {
+                printGreenGenericLn(
+                    "We are allowing the creation of an Access Control Context - createAccessControlContext");
+
+            }
+        };
+        System.setSecurityManager(setSecurityManager);
         final Permission perm1 = new SecurityPermission("createAccessControlContext");
         final PermissionCollection permissionCollection = perm1.newPermissionCollection();
         permissionCollection.add(perm1);
@@ -55,11 +67,16 @@ public class Crum31 {
                 printOrangeGenericLn(f2.apply(pathname));
                 return null;
             }
-        }, new AccessControlContext(new ProtectionDomain[] {
-            new ProtectionDomain(null, perms),
-            new ProtectionDomain(null, permissionCollection)
-        }));
+        }, new AccessControlContext(new ProtectionDomain[] { new ProtectionDomain(null, perms),
+            new ProtectionDomain(null, permissionCollection) }));
 
+        try {
+            System.setSecurityManager(securityManager);
+        } catch (AccessControlException e) {
+            printRedGeneric(
+                "This is expected! We have changed permissions in such a way that we can no longer change the SecurityManqger during runtime. Safe! -> %s",
+                e);
+        }
         printGreenGenericLn(
             "Make sure to study this code. This is how you make sure to reduce privileges from a high privileged user on demand");
     }
