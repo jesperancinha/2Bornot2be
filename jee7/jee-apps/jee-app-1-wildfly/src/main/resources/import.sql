101c1412575ca346e234b6443e565ca9514be02c
Joao Esperancinha <jofisaes@gmail.com>
2020-12-27 17:37:56 +0100

    ⚔️ Mastery JEE App1 - 11. More data

--- jee7/jee-apps/jee-app-1-wildfly/src/main/resources/import.sql
+++ jee7/jee-apps/jee-app-1-wildfly/src/main/resources/import.sql
@@ -1,1 +1,2 @@
 INSERT INTO Album (id,artist,albumName,year) VALUES (1,'Cardi B','Invasion of Privacy', 2018);
+INSERT INTO Album (id,artist,albumName,year) VALUES (2,'Nicky Minaj','Pink Friday', 2010);
