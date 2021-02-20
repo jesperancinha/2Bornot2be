6d0ff19a9537b5024609b5eb9ab051803bf02dd2
Joao Esperancinha <jofisaes@gmail.com>
2021-02-20 11:58:02 +0100

    ⚔️ Mastery JEE - jee-app-3-wildfly - Refactoring

--- java-test-11/java-11-basics/java-11-basics-2/dawj-driver/src/main/java/org/jesperancinha/java11/db/driver/DbDriver.java
+++ java-test-11/java-11-basics/java-11-basics-2/dawj-driver/src/main/java/org/jesperancinha/java11/db/driver/DbDriver.java
@@ -1,17 +1,17 @@
 package org.jesperancinha.java11.db.driver;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 
+import static org.jesperancinha.console.consolerizer.ConsolerizerColor.MAGENTA;
 import static org.jesperancinha.console.consolerizer.ConsolerizerColor.RED;
 
 public class DbDriver {
     public static void main(String[] args) {
         try {
-            Connection conn = DriverManager.
+            final var conn = DriverManager.
                     getConnection("jdbc:h2:mem:", "sa", "");
-            System.out.printf("Connection: %s / %s\n", conn.getMetaData().getDatabaseProductName(), conn.getCatalog());
-            System.out.printf("Current auto-commit setting: %s\n", conn.getAutoCommit());
+            MAGENTA.printGenericLn("Connection: %s / %s\n", conn.getMetaData().getDatabaseProductName(), conn.getCatalog());
+            MAGENTA.printGenericLn("Current auto-commit setting: %s\n", conn.getAutoCommit());
             conn.close();
         } catch (Exception e) {
             RED.printThrowableAndExit(e);
